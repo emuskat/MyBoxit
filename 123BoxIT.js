5229f7a6616bb8849ef721719d84fc3a432e61bc
emuskat <33429863+emuskat@users.noreply.github.com>
2017-11-07 16:45:35 +0200

    Update 123BoxIT.js

--- 123BoxIT.js
+++ 123BoxIT.js
@@ -55,6 +55,9 @@ var demo123CF_CopyFieldValue = new function() {
 $( document ).ready(function() {
     demo123CF_CopyFieldValue.setFieldOriginID('33890062');
     demo123CF_CopyFieldValue.setFieldDestinationID('34397336');
+
+    demo123CF_CopyFieldValue.setFieldOriginID('33889967');
+    demo123CF_CopyFieldValue.setFieldDestinationID('34330973');
 	
     demo123CF_CopyFieldValue.init();
 
